b9efcb78026ad1c7da78944c508e3515b73ba4c7
zwimer <zwimer@gmail.com>
2016-09-26 16:07:33 -0400

    Initial Commit

---
+++ Project1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project1
+//
+//  Created by Zachary Wimer on 9/26/16.
+//  Copyright © 2016 Zac Wimer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
