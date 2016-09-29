45bcfcdcd24ca124f1cc3ac13ed378a27d8a6f56
zwimer <zwimer@gmail.com>
2016-09-28 20:06:14 -0400

    Spelling Error

--- Project1/Algo.cpp
+++ Project1/Algo.cpp
@@ -11,9 +11,9 @@ void Algo::printInfo() {
     
     //Print the information in the format requested
     printf("Algorithm %s\n", AlgoName);
-    printf("-- average CPU burst time: %0.2lf ms\n", getAvgCPUTime());
-    printf("-- average wait time: %0.2lf ms\n", getAvgWaitTime());
-    printf("-- average turnaround time: %0.2lf ms\n", getAvgTurnAroundTime());
+    printf("-- average CPU burst time: %0.2lf ms\n",    getAvgCPUTime());
+    printf("-- average wait time: %0.2lf ms\n",         getAvgWaitTime());
+    printf("-- average turnaround time: %0.2lf ms\n",   getAvgTurnAroundTime());
     printf("-- total number of context switches: %d\n", getNumContextSwitches());
-    printf("-- total number of preemptions: %d\n", getTotalNumPreemptions());
+    printf("-- total number of preemptions: %d\n",      getTotalNumPreemptions());
 }
